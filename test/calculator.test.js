39404250d976962250bf103e9cb531b8a99634e9
Jina Kim(김진아) <164317584+jinaaaaaaaaaaaaa@users.noreply.github.com>
2026-05-11 23:21:30 +0900

    Revert "[Feat] CI/CD & PR 검증 자동화"

--- test/calculator.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "vitest";
-import { add } from "../src/calculator.js";
-
-describe("add 함수 테스트", () => {
-  test("두 숫자를 더한 값을 반환한다", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
